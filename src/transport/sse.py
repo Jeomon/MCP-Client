ce666b2480f8b7847848f996a5d9cf73addd6a1e
Jeomon <jeogeoalukka@gmail.com>
2025-06-06 17:04:15 +0530

    added SSE support

--- src/transport/sse.py
+++ src/transport/sse.py
@@ -1,6 +1,7 @@
 from src.types.json_rpc import JSONRPCRequest, JSONRPCError, JSONRPCResponse
 from src.transport.base import BaseTransport
 from httpx import AsyncClient, Limits
+from src.exception import MCPError
 from typing import Optional
 import json
 
@@ -21,14 +22,19 @@ class SSETransport(BaseTransport):
         '''
         self.client=AsyncClient(timeout=30,headers=self.headers,limits=Limits(max_connections=10))
 
-    async def disconnect(self):
-        '''
-        Close the Http Client
-        '''
-        if self.client:
-            await self.client.aclose()
-
     async def send_request(self, request:JSONRPCRequest):
+        '''
+        Send a JSON RPC request to the MCP server
+
+        Args:
+            request: JSON RPC request object
+
+        Returns:
+            JSON RPC response object
+        
+        Raises:
+            MCPError: If the request fails
+        '''
         headers={
             **self.headers,
             "Content-Type": "application/json",
@@ -44,14 +50,23 @@ class SSETransport(BaseTransport):
                         data:dict=json.loads(line[6:])
                         if data.get("id")==id:
                             if data.get("result"):
-                                return JSONRPCResponse.model_validate(data)
+                                message=JSONRPCResponse.model_validate(data)
+                                return message
                             elif data.get("error"):
-                                return JSONRPCError.model_validate(data)
+                                message=JSONRPCError.model_validate(data)
+                                error=message.error
+                                raise MCPError(code=error.code,message=error.message)
                     except json.JSONDecodeError:
                         print(f"Invalid JSON received: {line}")
         raise Exception(f"Request timed out for request id: {id}")
 
     async def send_notification(self, notification:JSONRPCResponse):
+        '''
+        Send a JSON RPC notification to the MCP server
+
+        Args:
+            notification: JSON RPC notification object
+        '''
         headers={
             **self.headers,
             "Content-Type": "application/json",
@@ -60,3 +75,9 @@ class SSETransport(BaseTransport):
         async with self.client.post(self.url,headers=headers,json=json_payload) as response:
             response.raise_for_status()
 
+    async def disconnect(self):
+        '''
+        Close the Http Client
+        '''
+        if self.client:
+            await self.client.aclose()
