cb13769a09f312b7ed7dcd9c2e87a7e96db9077d
Jeomon <jeogeoalukka@gmail.com>
2025-06-07 17:23:03 +0530

    added the websocket transport

--- src/client/utils.py
+++ src/client/utils.py
@@ -1,5 +1,6 @@
 from src.transport.stdio import StdioTransport,StdioServerParams
 from src.transport.streamable_http import StreamableHTTPTransport
+from src.transport.websocket import WebSocketTransport
 from src.transport.sse import SSETransport
 from src.transport.base import BaseTransport
 from typing import Any
@@ -16,13 +17,15 @@ def create_transport_from_server_config(server_config:dict[str,Any])->BaseTransp
     '''
     if is_sse_transport(server_config):
         return SSETransport(**server_config)
-    if is_stdio_transport(server_config):
+    elif is_stdio_transport(server_config):
         params=StdioServerParams(**server_config)
         return StdioTransport(params=params)
-    if is_streamable_http_transport(server_config):
+    elif is_streamable_http_transport(server_config):
         return StreamableHTTPTransport(**server_config)
-    
-    raise ValueError(f'Invalid server configuration: {server_config}')
+    elif is_websocket_transport(server_config):
+        return WebSocketTransport(**server_config)
+    else:
+        raise ValueError(f'Invalid server configuration: {server_config}')
 
 
 def is_sse_transport(server_config:dict[str,Any])->bool:
@@ -33,4 +36,7 @@ def is_streamable_http_transport(server_config:dict[str,Any])->bool:
 
 def is_stdio_transport(server_config:dict[str,Any])->bool:
     return 'command' in server_config and 'args' in server_config
+
+def is_websocket_transport(server_config:dict[str,Any])->bool:
+    return 'url' in server_config and 'ws' in server_config.get('url')
     
