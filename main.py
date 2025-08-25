c1ecb4f537313c4f51b91efc6369c0e566be5c00
Jeomon <jeogeoalukka@gmail.com>
2025-08-25 23:46:31 +0530

    removed demo.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ async def main():
     session=await client.create_session('windows-mcp')
     print(session.get_initialize_result())
     # sleep(5)
-    # response=await session.tools_call(tool_name="State-Tool")
-    # print(response.content[0].text)
+    response=await session.tools_call(tool_name="State-Tool")
+    print(response.content[0].text)
     await client.close_session('windows-mcp')
 
 if __name__ == '__main__':
